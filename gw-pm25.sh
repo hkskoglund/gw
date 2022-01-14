11671e489f2ed6caa3848308183663d7ec52a76b
Henning Skoglund <hkskoglund@gmail.com>
2022-01-14 12:27:55 +0100

    Added rainintensity styling

--- gw-pm25.sh
+++ gw-pm25.sh
@@ -3,25 +3,31 @@
 #added space at end when doing when refreshing screen with printf \e[H, otherwise status is merged if changed
                               PM25_AQI_GOOD=${PM25_AQI_GOOD:="GOOD         "}
                       PM25_AQI_MODERATE=${PM25_AQI_MODERATE:="MODERATE     "}
-PM25_AQI_UNHEALTHY_SENSITIVE=${PM25_AQI_UNHEALTHY_SENSITIVE:="UNHEALTHY S."}
-                      PM25_AQI_UNHEALTHY=${PM25_AQI_UNHEALTHY:="UNHEALTY     "}
-            PM25_AQI_VERY_UNHEALTHY=${PM25_AQI_VERY_UNHEALTHY:="VERY UNHEALTY"}
+PM25_AQI_UNHEALTHY_SENSITIVE=${PM25_AQI_UNHEALTHY_SENSITIVE:="UNHEALTHY S. "}
+                    PM25_AQI_UNHEALTHY=${PM25_AQI_UNHEALTHY:="UNHEALTY     "}
+          PM25_AQI_VERY_UNHEALTHY=${PM25_AQI_VERY_UNHEALTHY:="VERY UNHEALTY"}
                     PM25_AQI_HAZARDOUS=${PM25_AQI_HAZARDOUS:="HAZARDOUS    "}
+
+               PM25_AQI_GOOD_LIMIT=121
+           PM25_AQI_MODERATE_LIMIT=355
+PM25_AQI_UNHEALTHY_SENSITIVE_LIMIT=555
+         PM25_AQI_UNHEALTHY_LIMIT=1505
+    PM25_AQI_VERY_UNHEALTHY_LIMIT=2505
 
 setAQI()
 #https://blissair.com/what-is-pm-2-5.htm
 {
-    if [ "$1" -lt 121 ]; then
+    if [ "$1" -lt $PM25_AQI_GOOD_LIMIT ]; then
       VALUE_PM25_AQI=$PM25_AQI_GOOD
-    elif [ "$1" -ge 121 ] && [ "$1" -lt 355 ]; then
+    elif [ "$1" -ge $PM25_AQI_GOOD_LIMIT ] && [ "$1" -lt $PM25_AQI_MODERATE_LIMIT ]; then
       VALUE_PM25_AQI=$PM25_AQI_MODERATE
-    elif [ "$1" -ge 355 ] && [ "$1" -lt 555 ]; then
+    elif [ "$1" -ge $PM25_AQI_MODERATE_LIMIT ] && [ "$1" -lt $PM25_AQI_UNHEALTHY_SENSITIVE_LIMIT ]; then
       VALUE_PM25_AQI=$PM25_AQI_UNHEALTHY_SENSITIVE
-    elif [ "$1" -ge 555 ] && [ "$1" -lt 1505 ]; then
+    elif [ "$1" -ge $PM25_AQI_UNHEALTHY_SENSITIVE_LIMIT ] && [ "$1" -lt $PM25_AQI_UNHEALTHY_LIMIT ]; then
       VALUE_PM25_AQI=$PM25_AQI_UNHEALTHY
-    elif [ "$1" -ge 1505 ] && [ "$1" -lt 2505 ]; then
+    elif [ "$1" -ge $PM25_AQI_UNHEALTHY_LIMIT ] && [ "$1" -lt $PM25_AQI_VERY_UNHEALTHY_LIMIT ]; then
       VALUE_PM25_AQI=$PM25_AQI_VERY_UNHEALTHY
-    elif [ "$1" -ge 2505 ]; then
+    elif [ "$1" -ge $PM25_AQI_VERY_UNHEALTHY_LIMIT ]; then
       VALUE_PM25_AQI=$PM25_AQI_HAZARDOUS
     fi
 }
@@ -30,15 +36,15 @@ setStyleAQI()
 {
     if [ "$1" -lt 121 ]; then
        STYLE_AQI="$STYLE_PM25_AQI_GOOD"
-    elif [ "$1" -ge 121 ] && [ "$1" -lt 355 ]; then
+    elif [ "$1" -ge 121 ] && [ "$1" -lt $PM25_AQI_MODERATE_LIMIT ]; then
         STYLE_AQI=$STYLE_PM25_AQI_MODERATE
-    elif [ "$1" -ge 355 ] && [ "$1" -lt 555 ]; then
+    elif [ "$1" -ge $PM25_AQI_MODERATE_LIMIT ] && [ "$1" -lt $PM25_AQI_UNHEALTHY_SENSITIVE_LIMIT ]; then
         STYLE_AQI=$STYLE_PM25_AQI_UNHEALTHY_SENSITIVE
-    elif [ "$1" -ge 555 ] && [ "$1" -lt 1505 ]; then
+    elif [ "$1" -ge $PM25_AQI_UNHEALTHY_SENSITIVE_LIMIT ] && [ "$1" -lt $PM25_AQI_UNHEALTHY_LIMIT ]; then
         STYLE_AQI=$STYLE_PM25_AQI_UNHEALTHY
-    elif [ "$1" -ge 1505 ] && [ "$1" -lt 2505 ]; then
+    elif [ "$1" -ge $PM25_AQI_UNHEALTHY_LIMIT ] && [ "$1" -lt $PM25_AQI_VERY_UNHEALTHY_LIMIT ]; then
         STYLE_AQI=$STYLE_PM25_AQI_VERY_UNHEALTHY
-    elif [ "$1" -ge 2505 ]; then
+    elif [ "$1" -ge $PM25_AQI_VERY_UNHEALTHY_LIMIT ]; then
        #STYLE_AQI="$CSI$SGI_FOREGC$SGI_COLOR_WHITE;${SGI_BACKGC_24BIT}126;0;35m" #7f0023
        STYLE_AQI=$STYLE_PM25_AQI_HAZARDOUS
     fi
