4a0e8f2d0d7f522bdd94d29f3884aa5da18abe80
Henning Skoglund <hkskoglund@gmail.com>
2022-04-25 11:12:50 +0200

    fix: use appendbuffer, date -R (rfc-5322)

--- test/webserver.sh
+++ test/webserver.sh
@@ -1,54 +1,54 @@
 #!/bin/sh
 #https://www.my-tiny.net/L19-netcat.htm
 
-HTTP_RESPONSE_200_OK="HTTP/1.1 200" # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
-HTTP_RESPONSE_404_NOTFOUND="HTTP/1.1 404"
-HTTP_RESPONSE_501_NOTIMPLEMENTED="HTTP/1.1 501"
+HTTP_RESPONSE_200_OK="200" # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
+HTTP_RESPONSE_404_NOTFOUND="404"
+HTTP_RESPONSE_501_NOTIMPLEMENTED="501"
 
 if [ -n "$ZSH_VERSION" ]; then
     #https://zsh.sourceforge.io/FAQ/zshfaq03.html
        setopt shwordsplit  #zsh compability for "1 2 3" -> split in 1 2 3
     fi
 
+. ../lib/appendBuffer.sh
 . ../lib/http.sh
 . ../lib/util.sh
 
 sendHttpResponse()
 {
-    printf "%b" "$APPEND_HTTP_RESPONSE" 
-    printf "%b" "$APPEND_HTTP_RESPONSE" >&2
+    printAppendBuffer
 }
 
 appendHttpResponseHeader()
 {
-    APPEND_HTTP_RESPONSE="${APPEND_HTTP_RESPONSE}$1: $2\r\n"
+    appendBuffer "%s: %s\r\n" "'$1' '$2'"
 }
 
 appendHttpDefaultHeaders()
 {
-   # appendHttpResponseHeader "Date" "$(date)"
+    appendHttpResponseHeader "Date" "$(date -R)"
+    #https://httpwg.org/specs/rfc7231.html#http.date
     appendHttpResponseHeader "Server" "gw"
-   # appendHttpResponseHeader "Connection" "close"
 }
 
-appendHttpResponseNewline()
+appendHttpResponseCRLF()
 {
-    APPEND_HTTP_RESPONSE="$APPEND_HTTP_RESPONSE\r\n"
+    appendFormat "\r\n"
 }
 
 appendHttpResponseCode()
 {
-    APPEND_HTTP_RESPONSE="${APPEND_HTTP_RESPONSE}$1\r\n"
+    appendBuffer "HTTP/1.1 %s\r\n" "'$1'"
 }
 
 appendHttpResponseBody()
 {
-    APPEND_HTTP_RESPONSE="$APPEND_HTTP_RESPONSE$1"
+    appendBuffer "%s" "'$1'"
 }
 
 resetHttpResponse()
 {
-    unset APPEND_HTTP_RESPONSE
+    resetAppendBuffer
 }
 
 sendHttpResponseCode()
@@ -58,7 +58,7 @@ sendHttpResponseCode()
     appendHttpResponseCode "$1"
     appendHttpDefaultHeaders
     appendHttpResponseHeader "Content-Length" "0"
-    appendHttpResponseNewline
+    appendHttpResponseCRLF
     sendHttpResponse
 }
 
@@ -117,9 +117,9 @@ webserver()
 
             GET)   case "$HTTP_REQUEST_ABSPATH" in
 
-                    /)                      #resetHttpResponse
-                                            appendHttpResponseCode "$HTTP_RESPONSE_200_OK"
-                                            appendHttpDefaultHeaders
+                        /)                   
+                                        appendHttpResponseCode "$HTTP_RESPONSE_200_OK"
+                                        appendHttpDefaultHeaders
 
                                         # shellcheck disable=SC2154
                                         case "$HTTP_HEADER_accept" in
@@ -133,51 +133,55 @@ webserver()
                                                 #https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
                                                 # for cross-origin request: 127.0.0.1:3000 Live Preview visual studio code -> webserver localhost:8000
                                                 appendHttpResponseHeader "Access-Control-Allow-Origin" "*"
-                                                appendHttpResponseNewline
-                                                sendHttpResponse
+                                                appendHttpResponseCRLF
                                                 echo >&2 Sending JSON
-                                                printf "%s" "$l_response_JSON"
+                                                #printf "%s" "$l_response_JSON"
+                                                appendHttpResponseBody "$l_repsponse_JSON"
+                                                sendHttpResponse
+
                                                 #problem WSL2: stty: 'standard input': Inappropriate ioctl for device
                                                 unset l_response_JSON
                                                 ;;
                                                    
                                             *text/html*)
+                                                    
                                                     echo >&2 "sending text/html"
                                                     appendHttpResponseHeader "Content-Type" "text/html"
                                                     getFilesize "$HTTP_SERVER_ROOT/index.html"
                                                     appendHttpResponseHeader "Content-Length" "$VALUE_FILESIZE"
 
-                                                    appendHttpResponseNewline
+                                                    appendHttpResponseCRLF
                                                     sendHttpResponse
                                                     cat "$HTTP_SERVER_ROOT/index.html"
                                                     ;;
                                             
-                                            *)      #appendHttpResponseHeader "Content-Type" "text/plain"
-                                                    ltextplain='hello from webserver'
-                                                    #appendHttpResponseHeader "Content-Length: ${#ltextplain}"
-                                                    #appendHttpResponseNewline
-                                                    #appendHttpResponseBody "$ltextplain"
-                                                    #sendHttpResponse
-                                                    echo >&2 "webserver: sending plain text"
-                                                    printf "HTTP/1.1 200\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s" ${#ltextplain} "$ltextplain"
-
+                                            *text/plain*|*)
+                                                    
+                                                    ltextplain=$( cd .. ; ./gw -g 192.168.3.16 -c l )
+                                                    getUnicodeStringLength "$ltextplain"
+                                                    appendHttpResponseHeader "Content-Type" "text/plain"
+                                                    appendHttpResponseHeader "Content-Length" "$(( VALUE_UNICODE_STRING_LENGTH + 2))" # 2=CRLF
+                                                    appendHttpResponseCRLF
+                                                    appendHttpResponseBody "$ltextplain"
+                                                    appendHttpResponseCRLF
+                                                    sendHttpResponse
+                                                    
                                                     ;;
                                         esac
                                     ;;
                                         
                         *".js")
-                                set -x
                                 l_script_file=${HTTP_REQUEST_ABSPATH##*/}
                                 l_script_dir=${HTTP_REQUEST_ABSPATH%"$l_script_file"}
                                 l_server_file="$HTTP_SERVER_ROOT$l_script_dir$l_script_file"
-                                set +x
+
                                 if [ -s "$l_server_file" ]; then
                                         appendHttpResponseCode "$HTTP_RESPONSE_200_OK"
                                         appendHttpDefaultHeaders
                                         appendHttpResponseHeader "Content-Type" "application/javascript"
                                         getFilesize "$l_server_file"
                                         appendHttpResponseHeader "Content-Length" "$VALUE_FILESIZE"
-                                        appendHttpResponseNewline
+                                        appendHttpResponseCRLF
                                         sendHttpResponse
                                         >&2 echo "webserver: sending javascript $l_server_file"
                                         cat "$l_server_file" 
@@ -185,6 +189,7 @@ webserver()
                                     >&2 echo "Error: script not found or empty: $l_server_file"
                                     sendHttpResponseCode "$HTTP_RESPONSE_404_NOTFOUND"
                                 fi
+                                
                                 unset l_script_file l_script_dir l_server_file
                             
                                 ;;
@@ -272,7 +277,8 @@ else
 # problem: sometimes transfer is stopped
 # curl -v  192.168.3.174:8000/lib/highcharts-v309.src.js
 #* transfer closed with 7016 bytes remaining to read
-# nc process is using sendto to write to socket, it keeps sending after webserver exit
+# nc process is using sendto to write to socket, it keeps sending after webserver process exits
+# strace -f -p $(pgrep nc) 2>&1
 # using sleep to let nc have some time to send all data
   
   sleep 1
